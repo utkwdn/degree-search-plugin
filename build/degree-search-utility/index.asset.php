076c88aef48ddb0abf215774811d59b56ef449a9
Corbin Schappel <cschappe@utk.edu>
2025-03-19 16:13:06 -0400

    chore(misc): remove build folder, update gitignore

--- build/degree-search-utility/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd0f0abd2dcb196ebf4ab');
--- build/degree-search-utility/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => 'bd72906cb888dd67d83d');
