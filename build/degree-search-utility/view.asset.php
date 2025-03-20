450cc7d1475334a2c7e988fbb40081bd0b172a77
Mark Naylor <marknaylor@macbookpro.lan>
2025-03-20 08:10:31 -0500

    chore(misc): remove build folder, update gitignore

--- build/degree-search-utility/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch'), 'version' => '259a5647536611c9fb2b');
--- build/search-widget/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '0573ebb92446bd004b6f');
