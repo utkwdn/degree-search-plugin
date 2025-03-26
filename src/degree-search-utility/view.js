bb67377bd08b6e0be8a53cd6439c691cc13fd414
Corbin Schappel <cschappe@utk.edu>
2025-03-26 12:21:09 -0400

    style(degree-search): fix input and tooltip styles, formatting changes

--- src/degree-search-utility/view.js
+++ src/degree-search-utility/view.js
@@ -33,7 +33,7 @@ export default function View() {
     // Possibly replace with default Bootstrap from UTKWDS
     const TooltipEl = ({ id, children, title }) => (
         <OverlayTrigger overlay={<Tooltip id={id}>{title}</Tooltip>}>
-            <span>{children}</span>
+            <span className="tooltip-icon">{children}</span>
         </OverlayTrigger>
     );
 
@@ -219,14 +219,14 @@ export default function View() {
     // Show back to top element
     useEffect(() => {
         const toggleVisibility = () => {
-          setIsBackToVisible(window.scrollY > 1200);
+            setIsBackToVisible(window.scrollY > 1200);
         };
-        
+
         window.addEventListener("scroll", toggleVisibility);
         return () => window.removeEventListener("scroll", toggleVisibility);
-      }, []);
-    
-      const scrollToElement = () => {
+    }, []);
+
+    const scrollToElement = () => {
         const element = document.getElementById("filters");
         if (element) {
             element.scrollIntoView({ behavior: "smooth" });
@@ -239,11 +239,11 @@ export default function View() {
             if (stickyEl.current) {
                 const rect = stickyEl.current.getBoundingClientRect();
                 let offset = 0;
-                
+
                 if (document.body.classList.contains("admin-bar")) {
                     offset = 32;
                 }
-                
+
                 if (rect.top <= offset) {
                     setIsSticky(true);
                 } else {
@@ -299,7 +299,7 @@ export default function View() {
     const InfoIcon = () => {
         return (
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2"/>
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2" />
             </svg>
         );
     };
@@ -307,7 +307,7 @@ export default function View() {
     const ChevronUpIcon = () => {
         return (
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
+                <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z" />
             </svg>
         );
     };
@@ -320,7 +320,7 @@ export default function View() {
                     <div className="programs-filters-fields">
                         <div className="programs-filters-field">
                             <div class="form-floating">
-                                <input className='form-control' aria-label="Program Search" id="program-search" name="search" type="search" value={searchTerm} onChange={(e) => handleFilterChange('search', e.target.value, setSearchTerm)} placeholder="Find a program" />
+                                <input className="form-control" aria-label="Program Search" id="program-search" name="search" type="search" value={searchTerm} onChange={(e) => handleFilterChange('search', e.target.value, setSearchTerm)} placeholder="Find a program" />
                                 <label for="program-search">Find a Program</label>
                             </div>
                         </div>
