e79529ae4044d2f393328b77ca2d9aeb01684f01
Mark Naylor <marknaylor@macbookpro.lan>
2025-04-01 15:05:20 -0500

    fix(render.php): prevent redeclaration of render function in render.php

--- src/degree-search-utility/render.php
+++ src/degree-search-utility/render.php
@@ -3,9 +3,10 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
-function degree_search_utility_render_callback() {
-    
-    echo '<div class="areasContainer alignfull" id="filters"></div>';
+if (!function_exists('degree_search_utility_render_callback')) {
+    function degree_search_utility_render_callback() {
+        return '<div class="areasContainer alignfull" id="filters"></div>';
+    }
 }
 
 // Output the rendered HTML
--- src/search-widget/render.php
+++ src/search-widget/render.php
@@ -3,16 +3,18 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 
-function degree_search_widget_render_callback($attributes) {
-    // Get the selected area of study
-    $area_of_study = isset($attributes['areaOfStudy']) ? esc_attr($attributes['areaOfStudy']) : '';
+if (!function_exists('degree_search_widget_render_callback')) {
+    function degree_search_widget_render_callback($attributes) {
+        // Get the selected area of study
+        $area_of_study = isset($attributes['areaOfStudy']) ? esc_attr($attributes['areaOfStudy']) : '';
 
-    // Get the selected page ID and convert it to a URL
-    $degree_search_page = isset($attributes['degreeSearchPage']) ? intval($attributes['degreeSearchPage']) : 0;
-    $degree_search_url = $degree_search_page ? get_permalink($degree_search_page) : '';
+        // Get the selected page ID and convert it to a URL
+        $degree_search_page = isset($attributes['degreeSearchPage']) ? intval($attributes['degreeSearchPage']) : 0;
+        $degree_search_url = $degree_search_page ? get_permalink($degree_search_page) : '';
 
-    // Render the block container with the selected area and degree search URL as data attributes
-    return '<div id="degree-search-widget" data-area="' . $area_of_study . '" data-url="' . esc_url($degree_search_url) . '"></div>';
+        // Render the block container with the selected area and degree search URL as data attributes
+        return '<div id="degree-search-widget" data-area="' . $area_of_study . '" data-url="' . esc_url($degree_search_url) . '"></div>';
+    }
 }
 
 // Output the rendered HTML
