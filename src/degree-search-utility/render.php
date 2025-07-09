69ba249c1e1dfc457246cd1e7d502d73698f2ede
Mark Naylor <marknaylor@MacBookPro.attlocal.net>
2025-07-09 14:46:17 -0500

    chore(php): format php files

--- src/degree-search-utility/render.php
+++ src/degree-search-utility/render.php
@@ -1,13 +1,22 @@
 <?php
 /**
+ * Render callback for the Degree Search Utility block.
+ *
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ *
+ * @package DegreeSearchUtility
  */
 
-if (!function_exists('degree_search_utility_render_callback')) {
-    function degree_search_utility_render_callback() {
-        return '<div class="areasContainer alignfull" id="filters"></div>';
-    }
+if ( ! function_exists( 'degree_search_utility_render_callback' ) ) {
+	/**
+	 * Render the Degree Search Utility block.
+	 *
+	 * @return string Rendered HTML.
+	 */
+	function degree_search_utility_render_callback() {
+		return '<div class="areasContainer alignfull" id="filters"></div>';
+	}
 }
 
-// Output the rendered HTML
-echo degree_search_utility_render_callback();
+// Output the rendered HTML.
+echo wp_kses_post( degree_search_utility_render_callback() );
--- src/search-widget/render.php
+++ src/search-widget/render.php
@@ -1,21 +1,31 @@
 <?php
 /**
+ * Render callback for the Degree Search Widget block.
+ *
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ *
+ * @package DegreeSearchUtility
  */
 
-if (!function_exists('degree_search_widget_render_callback')) {
-    function degree_search_widget_render_callback($attributes) {
-        // Get the selected area of study
-        $area_of_study = isset($attributes['areaOfStudy']) ? esc_attr($attributes['areaOfStudy']) : '';
+if ( ! function_exists( 'degree_search_widget_render_callback' ) ) {
+	/**
+	 * Render the Degree Search Widget block.
+	 *
+	 * @param array $attributes Block attributes.
+	 * @return string Rendered HTML.
+	 */
+	function degree_search_widget_render_callback( $attributes ) {
+		// Get the selected area of study.
+		$area_of_study = isset( $attributes['areaOfStudy'] ) ? esc_attr( $attributes['areaOfStudy'] ) : '';
 
-        // Get the selected page ID and convert it to a URL
-        $degree_search_page = isset($attributes['degreeSearchPage']) ? intval($attributes['degreeSearchPage']) : 0;
-        $degree_search_url = $degree_search_page ? get_permalink($degree_search_page) : '';
+		// Get the selected page ID and convert it to a URL.
+		$degree_search_page = isset( $attributes['degreeSearchPage'] ) ? intval( $attributes['degreeSearchPage'] ) : 0;
+		$degree_search_url  = $degree_search_page ? get_permalink( $degree_search_page ) : '';
 
-        // Render the block container with the selected area and degree search URL as data attributes
-        return '<div id="degree-search-widget" data-area="' . $area_of_study . '" data-url="' . esc_url($degree_search_url) . '"></div>';
-    }
+		// Render the block container with the selected area and degree search URL as data attributes.
+		return '<div id="degree-search-widget" data-area="' . $area_of_study . '" data-url="' . esc_url( $degree_search_url ) . '"></div>';
+	}
 }
 
-// Output the rendered HTML
-echo degree_search_widget_render_callback($attributes);
+// Output the rendered HTML.
+echo wp_kses_post( degree_search_widget_render_callback( $attributes ) );
